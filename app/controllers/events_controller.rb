86912437d6054d0c8693c5e03dabd71c8a0f4af4
John Maguire <jmaguire5588@gmail.com>
2017-04-27 21:18:46 -0400

    adding horse race

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,8 @@ class EventsController < ApplicationController
   def index
     @first         = Event.where("date = ''").first
     @second        = Event.where("date = 'Every Monday in April with Brooklyn Caravan 7:00-9:00'").first
-    @events        = Event.where("date != '' AND date != 'Every Monday in April with Brooklyn Caravan 7:00-9:00'").sort_by{|x| x.date.to_date}
+    @horse         = Event.where("date = '5/6'")
+    @events        = Event.where("date != '' AND date != 'Every Monday in April with Brooklyn Caravan 7:00-9:00' AND date != '5/6'").sort_by{|x| x.date.to_date}
     needed         = 65000.00
     raised         = Money.find_by(name: 'Amount Raised')
     percent        = (raised.dollar/needed) * 100
