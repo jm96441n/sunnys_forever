2b2615fc2b99b7523123be2c90fe7ae981360286
John Maguire <jmaguire5588@gmail.com>
2017-04-27 20:55:38 -0400

    updated for dates to be ordered

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,15 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.order(:date)
-    needed = 65000.00
-    raised = Money.find_by(name: 'Amount Raised')
-    percent = (raised.dollar/needed) * 100
+    @first         = Event.where("date = ''").first
+    @second        = Event.where("date = 'Every Monday in April with Brooklyn Caravan 7:00-9:00'").first
+    @events        = Event.where("date != '' AND date != 'Every Monday in April with Brooklyn Caravan 7:00-9:00'").sort_by{|x| x.date.to_date}
+    needed         = 65000.00
+    raised         = Money.find_by(name: 'Amount Raised')
+    percent        = (raised.dollar/needed) * 100
     @amount_needed = "$65,000"
     @amount_raised = raised.dollar
-    @goal_percent = "#{percent.to_i}%"
+    @goal_percent  = "#{percent.to_i}%"
   end
 
 end
